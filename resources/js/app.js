c87eb4c4b9f6f1d92d00a7f930dc46a0ad94f9fb
Ehren Castillo <ehrencastillo@gmail.com>
2024-11-05 00:02:31 +0800

    refactor(app.js): set Layout.svelte as default layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,14 @@
+import Layout from '@shared/Layout.svelte';
 import { createInertiaApp } from '@inertiajs/svelte';
 
 createInertiaApp({
     id: 'app',
     resolve: (name) => {
         const pages = import.meta.glob('./pages/**/*.svelte', { eager: true });
-        return pages[`./pages/${name}.svelte`];
+        let page = pages[`./pages/${name}.svelte`];
+        return { default: page.default, layout: page.layout || Layout };
+        // Uses default layout unless a different the page has its own layout defined
+        // See: https://inertiajs.com/pages
     },
     setup({ el, App, props }) {
         new App({ target: el, props });
--- resources/js/pages/(authenticated)/account/Index.svelte
+++ resources/js/pages/(authenticated)/account/Index.svelte
@@ -1,5 +1,1 @@
-<script context="module">
-    export { default as layout } from '@shared/Layout.svelte';
-</script>
-
 <div>Account</div>
