b6aee988956033f9dddf020b818380ce49b648f7
BluNuggets <eoczer@gmail.com>
2024-11-05 12:34:00 +0800

    feat: add color scheme for website

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,32 @@ export default {
         './resources/**/*.{svelte,js,ts,jsx,tsx}',
     ],
     theme: {
+        colors: {
+            'mint' : '#C7EAD2',
+            'lighttint' : '#F1F1F1',
+            'green' : '#227A3D',
+            'darktint' : '#1E1E1E',
+            'blue' : {
+                light: '#b2c1d1',
+                DEFAULT: '#2663a6',
+            },
+            'forest' : {
+                light: '#adc8b5',
+                DEFAULT: '#227a3d',
+            },
+            'purple' : {
+                light: '#cbbce3',
+                DEFAULT: '#8054d0',
+            },
+            'brown' : {
+                light: '#cbc2b0',
+                DEFAULT: '#866b26',
+            },
+            'red' : {
+                light: '#dcafb2',
+                DEFAULT: '#bf2a30',
+            },
+        },
         extend: {},
     },
     plugins: [],
