c8c5d98eebf0053ca1863f3ea576993bcea3020f
BluNuggets <eoczer@gmail.com>
2024-11-05 13:12:08 +0800

    feat: add Navbar UI

--- resources/js/shared/components/Navbar.svelte
+++ resources/js/shared/components/Navbar.svelte
@@ -1,12 +1,59 @@
 <script>
     import { inertia } from '@inertiajs/svelte';
+    import Home from '@assets/home_logo.svg';
+    import Dashboard from '@assets/dashboard_logo.svg';
+    import Privacy from '@assets/privacy_logo.svg';
+    import Account from '@assets/account_logo.svg';
 </script>
 
-<nav class="bg-slate-100">
+<nav class="
+    bg-mint
+    w-24
+    text-center
+">
     <ul>
-        <a use:inertia href="/"><li>Home</li></a>
-        <a use:inertia href="/dashboard"><li>Dashboard</li></a>
-        <a use:inertia href="/privacy"><li>Privacy</li></a>
-        <a use:inertia href="/account"><li>Account</li></a>
+        <a use:inertia href="/">
+            <li class="
+                py-4 my-1
+            ">
+                <div class="flex justify-center ">
+                    <img alt="Home Logo" src={ Home } class="w-10"/>
+                </div>
+                Home
+            </li>
+        </a>
+
+        <a use:inertia href="/dashboard">
+            <li class="
+                py-4 my-1
+            ">
+                <div class="flex justify-center ">
+                    <img alt="Dashboard Logo" src={ Dashboard } class="w-10"/>
+                </div>
+                Dashboard
+            </li>
+        </a>
+
+        <a use:inertia href="/privacy">
+            <li class="
+                py-4 my-1
+            ">
+                <div class="flex justify-center ">
+                    <img alt="Privacy Logo" src={ Privacy } class="w-10"/>
+                </div>
+                Privacy
+            </li>
+        </a>
+
+        <a use:inertia href="/account">
+            <li class="
+                py-4 my-1
+            ">
+                <div class="flex justify-center ">
+                    <img alt="Account Logo" src={ Account } class="w-10"/>
+                </div>
+                Account
+            </li>
+        </a>
     </ul>
 </nav>
