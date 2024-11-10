653b5f6a5cbf234b25d3fb346a21c1a7c061ddb8
Ehren Castillo <ehrencastillo@gmail.com>
2024-11-10 19:54:06 +0800

    feat(ThemeSwitch.svelte): add color transition

--- resources/js/shared/components/ThemeSwitch.svelte
+++ resources/js/shared/components/ThemeSwitch.svelte
@@ -1,13 +1,21 @@
 <script>
     import LightMode from '@assets/lightmode_logo.svelte';
     import DarkMode from '@assets/darkmode_logo.svelte';
+    import { onMount } from 'svelte';
 
     export let icon = LightMode;
     let darkMode = true;
 
+    onMount(() => {
+        updateTheme();
+    });
+
     function handleSwitchMode() {
         darkMode = !darkMode;
+        updateTheme();
+    }
 
+    function updateTheme() {
         if (darkMode) {
             icon = DarkMode;
             document.documentElement.classList.add('dark');
@@ -23,8 +31,8 @@
     h-16 w-16
     cursor-pointer content-center
     justify-items-center text-light-primary-text
-    hover:text-light-secondary dark:text-dark-primary-text
-    dark:hover:text-dark-secondary"
+    transition-colors hover:text-light-secondary
+    dark:text-dark-primary-text dark:hover:text-dark-secondary"
     on:click={handleSwitchMode}
 >
     <svelte:component this={icon} />
