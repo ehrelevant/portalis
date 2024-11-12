9976a9bcb2aed465045b63f2824a524b00fe8bb4
BluNuggets <eoczer@gmail.com>
2024-11-12 10:48:06 +0800

    feat: add template for home page

--- resources/js/pages/home/Index.svelte
+++ resources/js/pages/home/Index.svelte
@@ -1,1 +1,58 @@
-<div class="main-screen text-9xl">Home</div>
+<script>
+    import Dashboard from '@assets/dashboard_logo.svelte'; 
+</script>
+
+<div class="main-screen p-4">
+    <div class = "flex flex-row justify-center sm:justify-start"> 
+        <!-- Temporary spot for logos-->
+        <div class="p-2">
+            <Dashboard />
+        </div>
+        <div class="p-2">
+            <Dashboard />
+        </div>
+    </div>
+    <div class="text-4xl font-semibold"> <i> Portalis: </i> CS195 Internship Portal </div>
+    <br>
+    <div class="text-md">
+        Welcome and thank you for diving into Portalis. Portalis is the dedicated portal for your CS 195 experience, granted to you by the University of the Philippines Diliman - Department of Computer Science. 
+        <br> <br>
+        This portal was created to ensure a streamlined process for every person involved, and that includes you!
+    </div>
+    <br>
+    <article>
+        <div class="text-xl italic"> Internship Phases </div>
+        <div class="text-md"> 
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Duis consectetur cursus libero, 
+            sed pretium erat venenatis ut. Vivamus iaculis justo in velit facilisis posuere. 
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        </div>
+    </article>
+    <br>
+    <article>
+        <div class="text-xl italic"> Getting Started </div>
+        <div class="text-md"> 
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Duis consectetur cursus libero, 
+            sed pretium erat venenatis ut. Vivamus iaculis justo in velit facilisis posuere. 
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        </div>
+    </article>
+    <br>
+    <article>
+        <div class="text-xl italic"> Acknowledgements </div>
+        <div class="text-md"> 
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. Duis consectetur cursus libero, 
+            sed pretium erat venenatis ut. Vivamus iaculis justo in velit facilisis posuere. 
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        </div>
+    </article>
+    <br>
+
+    <!--Some helpful buttons-->
+    <div> 
+        
+    </div>
+</div>
