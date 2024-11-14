60fe15e360cfe31667949f62776c07d82f31873f
Ehren Castillo <ehrencastillo@gmail.com>
2024-11-15 01:09:23 +0800

    fix: fix navbar shrinking when main content is large

--- resources/js/shared/components/Navbar.svelte
+++ resources/js/shared/components/Navbar.svelte
@@ -22,11 +22,11 @@
 <!-- Desktop Navbar -->
 <nav
     class="
-    sticky top-0 left-0 hidden
-    min-h-full max-h-screen
-    w-24 flex-col justify-between
-    bg-light-primary text-center
-    dark:bg-dark-primary sm:flex
+    sticky left-0 top-0 hidden
+    max-h-screen min-h-full
+    w-24 shrink-0 flex-col
+    justify-between bg-light-primary
+    text-center dark:bg-dark-primary sm:flex
 "
 >
     <ul class="flex flex-col">
@@ -44,43 +44,58 @@
 <!-- Mobile Navbar -->
 <nav
     class="
-    sticky flex top-0
-    min-w-screen
+    min-w-screen sticky top-0
+    flex
     h-24 flex-row
     justify-between
-    text-light-primary-text dark:text-dark-primary-text
-    bg-light-primary dark:bg-dark-primary 
+    bg-light-primary text-light-primary-text
+    dark:bg-dark-primary dark:text-dark-primary-text
     sm:hidden
-">
+"
+>
     {#if isMenuOpen}
         <div class="z-50 h-screen w-screen">
             <div>
-                <div class="flex flex-row h-24 justify-between content-center">
-                    <div class="p-4 content-center"> <p class="font-bold italic">Portalis:</p> CS195 Portal</div> 
-                    <button class="p-4 cursor-pointer" on:click={closeMenu}> <Close /> </button>
+                <div class="flex h-24 flex-row content-center justify-between">
+                    <div class="content-center p-4">
+                        <p class="font-bold italic">Portalis:</p>
+                         CS195 Portal
+                    </div>
+                    <button class="cursor-pointer p-4" on:click={closeMenu}>
+                        <Close />
+                    </button>
                 </div>
 
-                <div class="flex flex-col justify-between bg-light-primary/95 dark:bg-dark-primary/95">
-                    <ul class="flex flex-col w-full justify-center content-center text-center">
+                <div
+                    class="flex flex-col justify-between bg-light-primary/95 dark:bg-dark-primary/95"
+                >
+                    <ul
+                        class="flex w-full flex-col content-center justify-center text-center"
+                    >
                         <NavBtn href="/" Icon={Home}>Home</NavBtn>
-                        <NavBtn href="/dashboard" Icon={Dashboard}>Dashboard</NavBtn>
+                        <NavBtn href="/dashboard" Icon={Dashboard}
+                            >Dashboard</NavBtn
+                        >
                         <NavBtn href="/privacy" Icon={Privacy}>Privacy</NavBtn>
                         <NavBtn href="/account" Icon={Account}>Account</NavBtn>
                     </ul>
 
-                    <div class="flex justify-center items-center py-8">
+                    <div class="flex items-center justify-center py-8">
                         <Toggle />
                     </div>
                 </div>
             </div>
 
-            <div class="h-auto">
-            </div>
+            <div class="h-auto"></div>
         </div>
-	{:else}
-        <div class="p-4 content-center"> <p class="font-bold italic">Portalis:</p> CS195 Portal </div>
-        
-        <button class="p-4 cursor-pointer" on:click={openMenu}> <Menu /> </button>
-	{/if}
+    {:else}
+        <div class="content-center p-4">
+            <p class="font-bold italic">Portalis:</p>
+             CS195 Portal
+        </div>
 
+        <button class="cursor-pointer p-4" on:click={openMenu}>
+            <Menu />
+        </button>
+    {/if}
 </nav>
