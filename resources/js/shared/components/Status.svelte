708e4dedc7854548d6726c9a89f246ee9479b108
BluNuggets <eoczer@gmail.com>
2024-11-14 21:39:13 +0800

    fix: align status widths

--- resources/js/shared/components/Status.svelte
+++ resources/js/shared/components/Status.svelte
@@ -2,23 +2,25 @@
     export let s_type;
 </script>
 
+<div class="w-40">
 {#if s_type == 'Pending'}
-    <div class="flex flex-row w-stretch p-2 rounded-full bg-floating-brown-light text-floating-brown items-center">
+    <div class="flex flex-row w-stretch p-2 justify-between rounded-full bg-floating-brown-light text-floating-brown items-center">
         <div class="rounded-full w-8 h-8 pr-3 bg-floating-brown"></div>
         <p class="mx-2 font-medium text-xl"> {s_type} </p>
     </div>
 {/if}
 
 {#if s_type == 'Submitted'}
-    <div class="flex flex-row w-stretch p-2 rounded-full bg-floating-forest-light text-floating-forest items-center">
+    <div class="flex flex-row w-stretch p-2 justify-between rounded-full bg-floating-forest-light text-floating-forest items-center">
         <div class="rounded-full w-8 h-8 pr-3 bg-floating-forest"></div>
         <p class="mx-2 font-medium text-xl"> {s_type} </p>
     </div>
 {/if}
 
 {#if s_type == 'Validated'}
-    <div class="flex flex-row w-stretch p-2 rounded-full bg-floating-blue-light text-floating-blue items-center">
+    <div class="flex flex-row w-stretch p-2 justify-between rounded-full bg-floating-blue-light text-floating-blue items-center">
         <div class="rounded-full w-8 h-8 pr-3 bg-floating-blue"></div>
         <p class="mx-2 font-medium text-xl"> {s_type} </p>
     </div>
 {/if}
+</div>
