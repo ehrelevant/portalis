61901e188bc1800956ccd39f56e779ded9e0aab2
Ehren Castillo <ehrencastillo@gmail.com>
2024-11-15 17:48:18 +0800

    feat(report): clean up styling slightly using grids

--- resources/js/pages/dashboard/during/(supervisor)/report/Index.svelte
+++ resources/js/pages/dashboard/during/(supervisor)/report/Index.svelte
@@ -64,86 +64,117 @@
     <form on:submit|preventDefault={handleSubmit}>
         <div class="flex flex-col gap-4">
             <div>
-                <p>Non-Technical Criteria</p>
-                {#each students as student, i}
-                    {@const { last_name, first_name } = student}
-                    <label class="flex flex-row">
-                        {last_name}, {first_name}
+                <h2 class="my-4 text-2xl">Non-Technical Criteria</h2>
+                <div
+                    class="grid grid-cols-[auto,repeat(4,1fr)] items-center justify-center gap-x-4 gap-y-2"
+                >
+                    <p class="col-start-2 text-center">Work Ethic (10)</p>
+                    <p class="text-center">Attitude and Personality (10)</p>
+                    <p class="text-center">Attendance and Punctuality (10)</p>
+                    <p class="text-center">Respect for Authority (10)</p>
+                    {#each students as student, i}
+                        {@const { last_name, first_name } = student}
+                        <p>{last_name}, {first_name}</p>
                         <input
-                            class="border-2"
+                            class="border-2 p-2 text-light-primary-text"
                             type="number"
+                            max="10"
+                            min="0"
+                            required
                             bind:value={$form.evaluations[i].ratings[1]}
                         />
                         <input
-                            class="border-2"
+                            class="border-2 p-2 text-light-primary-text"
                             type="number"
+                            max="10"
+                            min="0"
+                            required
                             bind:value={$form.evaluations[i].ratings[2]}
                         />
                         <input
-                            class="border-2"
+                            class="border-2 p-2 text-light-primary-text"
                             type="number"
+                            max="10"
+                            min="0"
+                            required
                             bind:value={$form.evaluations[i].ratings[3]}
                         />
                         <input
-                            class="border-2"
+                            class="border-2 p-2 text-light-primary-text"
                             type="number"
+                            max="10"
+                            min="0"
+                            required
                             bind:value={$form.evaluations[i].ratings[4]}
                         />
-                    </label>
-                {/each}
+                    {/each}
+                </div>
             </div>
 
             <div>
-                <p>Technical Criteria</p>
-                {#each students as student, i}
-                    {@const { last_name, first_name } = student}
-                    <label class="flex flex-row">
-                        {last_name}, {first_name}
+                <h2 class="my-4 text-2xl">Technical Criteria</h2>
+                <div
+                    class="grid grid-cols-[auto,1fr] items-center justify-center gap-x-4 gap-y-2"
+                >
+                    <p class="col-start-2 text-center">Work Output (60)</p>
+                    {#each students as student, i}
+                        {@const { last_name, first_name } = student}
+                        <p>{last_name}, {first_name}</p>
                         <input
-                            class="border-2"
+                            class="border-2 p-2 text-light-primary-text"
                             type="number"
+                            max="60"
+                            min="0"
+                            required
                             bind:value={$form.evaluations[i].ratings[5]}
                         />
-                    </label>
-                {/each}
+                    {/each}
+                </div>
             </div>
 
             <div>
-                <p>Number of Hours</p>
-                {#each students as student, i}
-                    {@const { last_name, first_name } = student}
-                    <label class="flex flex-row">
-                        {last_name}, {first_name}
+                <h2 class="my-4 text-2xl">Number of Hours</h2>
+                <div
+                    class="grid grid-cols-[auto,1fr] items-center justify-center gap-x-4 gap-y-2"
+                >
+                    <p class="col-start-2 text-center">Total Hours</p>
+                    {#each students as student, i}
+                        {@const { last_name, first_name } = student}
+                        <p>{last_name}, {first_name}</p>
                         <input
-                            class="border-2"
+                            class="border-2 p-2 text-light-primary-text"
                             type="number"
+                            min="0"
+                            required
                             bind:value={$form.evaluations[i].hours}
                         />
-                    </label>
-                {/each}
+                    {/each}
+                </div>
             </div>
 
             <div>
-                <p>Comments or Concerns</p>
-                {#each students as student, i}
-                    {@const { last_name, first_name } = student}
-                    <label class="flex flex-row">
-                        {last_name}, {first_name}
+                <h2 class="my-4 text-2xl">Comments or Concerns</h2>
+                <div
+                    class="grid grid-cols-[auto,1fr] items-center justify-center gap-x-4 gap-y-2"
+                >
+                    {#each students as student, i}
+                        {@const { last_name, first_name } = student}
+                        <p>{last_name}, {first_name}</p>
                         <textarea
-                            class="border-2"
+                            class="w-full border-2 p-2 text-light-primary-text"
                             bind:value={$form.evaluations[i]['comments']}
                         />
-                    </label>
-                {/each}
+                    {/each}
+                </div>
             </div>
-        </div>
 
-        <div class="m-2 flex justify-center">
-            <input
-                type="submit"
-                value="Submit Response"
-                class="w-fit cursor-pointer border-2 bg-light-secondary p-4 text-3xl text-dark-primary-text hover:opacity-90"
-            />
+            <div class="m-2 flex justify-center">
+                <input
+                    type="submit"
+                    value="Submit Response"
+                    class="w-fit cursor-pointer border-2 bg-light-secondary p-4 text-3xl text-dark-primary-text hover:opacity-90"
+                />
+            </div>
         </div>
     </form>
 </div>
