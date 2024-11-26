c36b6f1a7a8ed2649b55fcae07273489ba0bc405
BluNuggets <eoczer@gmail.com>
2024-11-26 23:00:11 +0800

    chore(pre): stylize faculty dashboard

--- resources/js/pages/dashboard/pre/(faculty)/Index.svelte
+++ resources/js/pages/dashboard/pre/(faculty)/Index.svelte
@@ -1,21 +1,27 @@
 <script>
     import { Link } from '@inertiajs/svelte';
     import Header from '@shared/components/InternshipHeader.svelte';
+    import Account from '@assets/account_logo.svelte';
 </script>
 
 <div class="main-screen w-full p-4">
     <Header txt="Faculty Dashboard" />
 
-    <div class="flex flex-col">
+    <div class="flex flex-col sm:flex-row w-full">
         <Link
             href="/dashboard/pre/students"
-            class="my-1 flex justify-between rounded-xl bg-white p-3 dark:bg-black"
-            >Student List</Link
+            class="w-full flex flex-col items-center justify-center rounded-xl bg-white p-3 m-2 text-2xl dark:bg-black hover:opacity-80"
         >
+        <Account />
+        <p>Student List</p>
+        </Link>
+
         <Link
             href="/dashboard/pre/supervisors"
-            class="my-1 flex justify-between rounded-xl bg-white p-3 dark:bg-black"
-            >Supervisor List</Link
+            class="w-full flex flex-col items-center justify-center rounded-xl bg-white p-3 m-2 text-2xl dark:bg-black hover:opacity-80"
         >
+        <Account />
+        <p>Supervisor List</p>
+        </Link>
     </div>
 </div>
