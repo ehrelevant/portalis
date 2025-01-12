4dcc7c0524e66d168f16ffba2de133d55925883e
Ehren Castillo <ehrencastillo@gmail.com>
2025-01-13 04:22:56 +0800

    refactor(during/supervisor): use accordion component on dashboard page

--- resources/js/pages/dashboard/during/(supervisor)/Index.svelte
+++ resources/js/pages/dashboard/during/(supervisor)/Index.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Accordion from '@/js/shared/components/Accordion.svelte';
     import { Link } from '@inertiajs/svelte';
     import Header from '@shared/components/InternshipHeader.svelte';
 
@@ -9,57 +10,59 @@
     export let intern_evaluation_status;
 </script>
 
-<div class="main-screen flex w-full flex-col gap-8 p-4">
-    <div>
-        <Header txt="During Internship Phase" />
+<section class="main-screen flex w-full flex-col p-4">
+    <Header txt="During Internship Phase" />
+
+    <div class="flex flex-col gap-4">
         <h2 class="text-2xl">Company: {company_name}</h2>
-    </div>
+        <Accordion>
+            <h2 slot="summary" class="text-2xl">
+                Weekly Performance Evaluation
+            </h2>
+            <div class="flex flex-col gap-2">
+                {#each weekly_report_statuses as weekly_report_status}
+                    {@const { week, status } = weekly_report_status}
 
-    <div class="flex flex-col gap-2">
-        <h2 class="text-xl">Weekly Performance Evaluation</h2>
-        <div class="flex flex-col gap-2">
-            {#each weekly_report_statuses as weekly_report_status}
-                {@const { week, status } = weekly_report_status}
-
-                {#if status != 'pending'}
-                    <!-- Removes link if answered already -->
-                    <div
-                        class="flex flex-row justify-between rounded-xl bg-white p-4 hover:opacity-90 dark:bg-black"
-                    >
-                        <div class="flex items-center">Week {week}</div>
-                        <Status s_type={status} />
-                    </div>
-                {:else}
-                    <Link
-                        href="/dashboard/during/report/{week}"
-                        class="flex flex-row justify-between rounded-xl bg-white p-4 hover:opacity-90 dark:bg-black"
-                    >
-                        <div class="flex items-center">Week {week}</div>
-                        <Status s_type={status} />
-                    </Link>
-                {/if}
-            {/each}
-        </div>
-    </div>
-
-    <div class="flex flex-col gap-2">
-        <h2 class="text-xl">Final Report</h2>
-
-        {#if intern_evaluation_status != 'pending'}
-            <div
-                class="flex flex-row justify-between rounded-xl bg-white p-4 hover:opacity-90 dark:bg-black"
-            >
-                <div class="flex items-center">Final Report</div>
-                <Status s_type={intern_evaluation_status} />
+                    {#if status != 'pending'}
+                        <!-- Removes link if answered already -->
+                        <div
+                            class="flex flex-row justify-between rounded-xl bg-white p-4 hover:opacity-90 dark:bg-black"
+                        >
+                            <div class="flex items-center">Week {week}</div>
+                            <Status s_type={status} />
+                        </div>
+                    {:else}
+                        <Link
+                            href="/dashboard/during/report/{week}"
+                            class="flex flex-row justify-between rounded-xl bg-white p-4 hover:opacity-90 dark:bg-black"
+                        >
+                            <div class="flex items-center">Week {week}</div>
+                            <Status s_type={status} />
+                        </Link>
+                    {/if}
+                {/each}
             </div>
-        {:else}
-            <Link
-                href="/dashboard/during/final"
-                class="flex flex-row justify-between rounded-xl bg-white p-4 hover:opacity-90 dark:bg-black"
-            >
-                <div class="flex items-center">Final Report</div>
-                <Status s_type={intern_evaluation_status} />
-            </Link>
-        {/if}
+        </Accordion>
+
+        <Accordion>
+            <h2 slot="summary" class="text-2xl">Final Report</h2>
+
+            {#if intern_evaluation_status != 'pending'}
+                <div
+                    class="flex flex-row justify-between rounded-xl bg-white p-4 hover:opacity-90 dark:bg-black"
+                >
+                    <div class="flex items-center">Final Report</div>
+                    <Status s_type={intern_evaluation_status} />
+                </div>
+            {:else}
+                <Link
+                    href="/dashboard/during/final"
+                    class="flex flex-row justify-between rounded-xl bg-white p-4 hover:opacity-90 dark:bg-black"
+                >
+                    <div class="flex items-center">Final Report</div>
+                    <Status s_type={intern_evaluation_status} />
+                </Link>
+            {/if}
+        </Accordion>
     </div>
-</div>
+</section>
--- resources/js/shared/components/Accordion.svelte
+++ resources/js/shared/components/Accordion.svelte
@@ -8,7 +8,7 @@
 
 <div class="flex flex-col">
     <button
-        class="cursor-pointer bg-light-primary px-4 py-2 text-left dark:bg-dark-primary {open
+        class="cursor-pointer bg-light-primary p-4 text-left dark:bg-dark-primary {open
             ? 'rounded-t-xl'
             : 'rounded-xl'}"
         on:click={toggleAccordion}
