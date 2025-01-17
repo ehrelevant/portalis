e7f35a4c6b13551e6fb568c7085ff28127ab5b12
Ehren Castillo <ehrencastillo@gmail.com>
2025-01-18 05:05:59 +0800

    refactor(Status): rename variables and remove redundant code

--- resources/js/shared/components/Status.svelte
+++ resources/js/shared/components/Status.svelte
@@ -1,32 +1,28 @@
 <script>
-    import { toTitleCase } from '@shared/helpers';
-
-    export let s_type;
-
-    $: statusTypeCapitalized = toTitleCase(s_type);
+    export let type;
 </script>
 
 <div class="w-36">
-    {#if statusTypeCapitalized === 'Pending'}
+    {#if type === 'pending'}
         <div
             class="w-stretch flex flex-row items-center justify-between rounded-full bg-floating-brown-light p-2 text-floating-brown"
         >
             <div class="h-6 w-6 rounded-full bg-floating-brown pr-3"></div>
-            <p class="text-md mx-2 font-medium">{s_type}</p>
+            <p class="text-md mx-2 font-medium">{type}</p>
         </div>
-    {:else if statusTypeCapitalized === 'Submitted'}
+    {:else if type === 'submitted'}
         <div
             class="w-stretch flex flex-row items-center justify-between rounded-full bg-floating-forest-light p-2 text-floating-forest"
         >
             <div class="h-6 w-6 rounded-full bg-floating-forest pr-3"></div>
-            <p class="text-md mx-2 font-medium">{s_type}</p>
+            <p class="text-md mx-2 font-medium">{type}</p>
         </div>
     {:else}
         <div
             class="w-stretch flex flex-row items-center justify-between rounded-full bg-floating-blue-light p-2 text-floating-blue"
         >
             <div class="h-6 w-6 rounded-full bg-floating-blue pr-3"></div>
-            <p class="text-md mx-2 font-medium">{s_type}</p>
+            <p class="text-md mx-2 font-medium">{type}</p>
         </div>
     {/if}
 </div>
