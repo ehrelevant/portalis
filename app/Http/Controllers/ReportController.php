32a6c4555f5b321028b290e710e3d268424ebd3a
Ehren Castillo <ehrencastillo@gmail.com>
2025-01-19 03:37:35 +0800

    refactor(ReportController): rename weekly report controller and adjust variables

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,19 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\WeeklyReport;
-use App\Models\WeeklyReportAnswer;
-use App\Models\WeeklyReportRating;
-use App\Models\WeeklyReportStatus;
+use App\Models\Report;
+use App\Models\ReportAnswer;
+use App\Models\ReportRating;
+use App\Models\ReportStatus;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
-class WeeklyReportController extends Controller
+class ReportController extends Controller
 {
-    public function show(int $week)
+    public function show()
     {
         $supervisor_user = Auth::user();
 
@@ -27,14 +27,12 @@ class WeeklyReportController extends Controller
 
         return Inertia::render('dashboard/during/(supervisor)/report/Index', [
             'students' => $supervised_students,
-            'week' => $week,
         ]);
     }
 
     public function submit(Request $request): RedirectResponse
     {
         $form_values = $request->validate([
-            'week' => ['integer', 'numeric'],
             'evaluations.*.student_number' => ['integer', 'numeric'],
             'evaluations.*.ratings' => ['required', 'array'],
             'evaluations.*.ratings.*' => ['required', 'integer', 'numeric'],
@@ -46,29 +44,28 @@ class WeeklyReportController extends Controller
         $supervisor_user = Auth::user();
 
         foreach ($form_values['evaluations'] as $evaluation) {
-            $report_status = WeeklyReportStatus::where('supervisor_id', $supervisor_user->role_id)
+            $report_status = ReportStatus::where('supervisor_id', $supervisor_user->role_id)
                 ->where('student_number', $evaluation['student_number'])
-                ->where('week', $form_values['week'])
                 ->firstOrFail();
             $report_status->status = 'submitted';
             $report_status->save();
 
-            $new_report = new WeeklyReport();
-            $new_report->weekly_report_status_id = $report_status->id;
+            $new_report = new Report();
+            $new_report->report_status_id = $report_status->id;
             $new_report->total_hours = $evaluation['hours'];
             $new_report->save();
 
             foreach ($evaluation['ratings'] as $i => $score) {
-                $report_rating = new WeeklyReportRating();
-                $report_rating->weekly_report_id = $new_report->id;
+                $report_rating = new ReportRating();
+                $report_rating->report_id = $new_report->id;
                 $report_rating->rating_question_id = $i;
                 $report_rating->score = $score;
                 $report_rating->save();
             }
 
             foreach ($evaluation['open_ended'] as $i => $open_ended) {
-                $report_open_ended = new WeeklyReportAnswer();
-                $report_open_ended->weekly_report_id = $new_report->id;
+                $report_open_ended = new ReportAnswer();
+                $report_open_ended->report_id = $new_report->id;
                 $report_open_ended->open_ended_question_id = $i;
                 $report_open_ended->answer = $open_ended ?? '';
                 $report_open_ended->save();
