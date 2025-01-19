4f1e9d2fe15d74f2170dbcec5ee475644b57e152
Ehren Castillo <ehrencastillo@gmail.com>
2025-01-19 21:07:16 +0800

    refactor(StatusCell): clean up component styling

--- resources/js/pages/dashboard/(faculty)/students/StatusCell.svelte
+++ resources/js/pages/dashboard/(faculty)/students/StatusCell.svelte
@@ -4,26 +4,26 @@
     export let student_number;
     export let requirement_id;
     export let status;
+
+    const bgColor =
+        status === 'validated'
+            ? 'bg-floating-forest'
+            : status === 'submitted'
+              ? 'bg-floating-brown'
+              : status === 'rejected'
+                ? 'bg-floating-red'
+                : 'bg-gray-700';
 </script>
 
 <div class="flex items-center justify-center">
-    {#if status === 'validated'}
-        <div>
+    <div class="w-fit justify-center rounded-lg px-2 {bgColor}">
+        {#if status !== 'unsubmitted'}
             <Link
                 href="/dashboard/students/{student_number}/{requirement_id}"
-                class="bg-floating-blue">{status}</Link
+                class={bgColor}>{status}</Link
             >
-        </div>
-    {:else if status === 'submitted'}
-        <div>
-            <Link
-                href="/dashboard/students/{student_number}/{requirement_id}"
-                class="bg-floating-forest">{status}</Link
-            >
-        </div>
-    {:else}
-        <div class="w-fit justify-center rounded-lg bg-gray-700 px-2">
+        {:else}
             <p class="text-base">{status}</p>
-        </div>
-    {/if}
+        {/if}
+    </div>
 </div>
