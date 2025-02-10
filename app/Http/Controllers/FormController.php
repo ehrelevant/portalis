328ebeaea70061091607bf93ef9f0deec8295cbf
Ehren Castillo <ehrencastillo@gmail.com>
2025-02-10 12:35:12 +0800

    feat(FormController): add functions for intern evaluation form

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -641,7 +641,7 @@ class FormController extends Controller
             'opens.*' => ['nullable'],
         ]);
 
-        $this->updateCompanyEvaluationForm($form_values, $short_name);
+        $this->updateSelfEvaluationForm($form_values, $short_name);
 
         return redirect('/dashboard');
     }
@@ -658,7 +658,7 @@ class FormController extends Controller
             'opens.*' => ['nullable', 'string'],
         ]);
 
-        $this->updateCompanyEvaluationForm($form_values, $short_name);
+        $this->updateSelfEvaluationForm($form_values, $short_name);
 
         $user = Auth::user();
 
@@ -674,32 +674,228 @@ class FormController extends Controller
     // Intern Evaluation Forms
     private function getSavedInternEvaluationValues($form_answers, $rating_categories)
     {
+        $students = [];
 
+        foreach ($form_answers as $form_answer) {
+            $form_answer_id = $form_answer->form_answer_id;
+            $student_user_id = $form_answer->evaluated_user_id;
+
+            $student_info = DB::table('users')
+                ->where('id', $student_user_id)
+                ->select('id', 'first_name', 'last_name', 'role_id AS student_number')
+                ->firstOrFail();
+
+            $categorized_ratings = [];
+            foreach ($rating_categories as $category) {
+                $ratings = DB::table('rating_scores')
+                    ->where('form_answer_id', $form_answer_id)
+                    ->join('rating_questions', 'rating_questions.id', '=', 'rating_scores.rating_question_id')
+                    ->where('rating_questions.rating_category_id', $category->id)
+                    ->pluck('rating_scores.score', 'rating_scores.rating_question_id')
+                    ->toArray();
+
+                $categorized_ratings[$category->id] = $ratings;
+            }
+
+            $opens = DB::table('open_answers')
+                ->where('form_answer_id', $form_answer_id)
+                ->pluck('answer', 'open_question_id')
+                ->toArray();
+
+            $total_hours = DB::table('form_statuses')
+                ->where('user_id', $student_user_id)
+                ->join('form_answers', 'form_answers.form_status_id', '=', 'form_statuses.id')
+                ->join('rating_scores', 'rating_scores.form_answer_id', '=', 'form_answers.id')
+                ->where('rating_question_id', 14) // 14 = Total hours rating question
+                ->firstOrFail()
+                ->score;
+
+            $self_assessment_status = DB::table('form_statuses')
+                ->where('user_id', $student_user_id)
+                ->where('form_id', 4) // 4 = Intern self-evaluation form
+                ->firstOrFail()
+                ->status;
+
+            $students[$student_info->student_number] = [
+                'student_user_id' => $student_info->id,
+                'student_number' => $student_info->student_number,
+                'last_name' => $student_info->last_name,
+                'first_name' => $student_info->first_name,
+                'categorized_ratings' => $categorized_ratings,
+                'opens' => $opens,
+                'total_hours' => $total_hours,
+                'self_assessment_status' => $self_assessment_status,
+            ];
+        }
+
+        return $students;
     }
 
     public function answerInternEvaluationForm()
     {
+        $short_name = 'intern-evaluation';
+        $supervisor_user = Auth::user();
 
+        $form_status = $this->queryFormStatus($supervisor_user->id, $short_name)->firstOrFail();
+        $form_answers = $this->queryFormAnswers($supervisor_user->id, $short_name)->get();
+
+        if ($form_answers->isEmpty()) {
+            $supervised_student_user_ids = DB::table('users')
+                ->where('role', 'student')
+                ->join('students', 'students.student_number', '=', 'users.role_id')
+                ->where('supervisor_id', $supervisor_user->role_id)
+                ->pluck('users.id');
+
+            $supervised_self_evaluation_statuses = DB::table('users')
+                ->where('role', 'student')
+                ->join('students', 'students.student_number', '=', 'users.role_id')
+                ->where('supervisor_id', $supervisor_user->role_id)
+                ->join('form_statuses', 'form_statuses.user_id', '=', 'users.id')
+                ->where('form_id', 4)
+                ->leftJoin('form_answers', 'form_answers.form_status_id', '=', 'form_statuses.id')
+                ->whereNull('form_answers.id')
+                ->select('form_statuses.form_id', 'form_statuses.id AS form_status_id')
+                ->get();
+
+            foreach ($supervised_self_evaluation_statuses as $supervised_self_evaluation_status) {
+                $this->createForm($supervised_self_evaluation_status, [null]);
+            }
+
+            $this->createForm($form_status, $supervised_student_user_ids);
+            $form_answers = $this->queryFormAnswers($supervisor_user->id, $short_name)->get();;
+        }
+
+        $rating_categories = $this->getRatingCategories($form_status->form_id);
+        $students = $this->getSavedInternEvaluationValues($form_answers, $rating_categories);
+        $categorized_rating_questions = $this->getCategorizedRatingQuestions($form_status->form_id, $rating_categories);
+        $open_questions = $this->getOpenQuestions($form_status->form_id);
+        $form_info = $this->getFormInfo($form_status->form_id);
+
+        return Inertia::render('form/intern-evaluation/Answer', [
+            'students' => $students,
+            'rating_categories' => $rating_categories,
+            'categorized_rating_questions' => $categorized_rating_questions,
+            'open_questions' => $open_questions,
+            'form_info' => $form_info,
+        ]);
     }
 
-    public function viewInternEvaluationForm(int $student_number)
+    public function viewInternEvaluationForm(int $supervisor_id)
     {
+        $short_name = 'intern-evaluation';
+        $user_id = DB::table('users')
+            ->where('role', 'supervisor')
+            ->where('role_id', $supervisor_id)
+            ->firstOrFail()
+            ->id;
 
+        $form_status = $this->queryFormStatus($user_id, $short_name)->firstOrFail();
+        $form_answers = $this->queryFormAnswers($user_id, $short_name)->get();
+
+        $rating_categories = $this->getRatingCategories($form_status->form_id);
+        $students = $this->getSavedInternEvaluationValues($form_answers, $rating_categories);
+        $categorized_rating_questions = $this->getCategorizedRatingQuestions($form_status->form_id, $rating_categories);
+        $open_questions = $this->getOpenQuestions($form_status->form_id);
+        $form_info = $this->getFormInfo($form_status->form_id);
+
+        $status = DB::table('form_statuses')
+            ->where('user_id', $user_id)
+            ->where('form_id', $form_status->form_id)
+            ->firstOrFail()
+            ->status;
+
+        return Inertia::render('form/intern-evaluation/View', [
+            'evaluator_user_id' => $user_id,
+            'students' => $students,
+            'rating_categories' => $rating_categories,
+            'categorized_rating_questions' => $categorized_rating_questions,
+            'open_questions' => $open_questions,
+            'form_info' => $form_info,
+            'status' => $status,
+        ]);
     }
 
     public function updateInternEvaluationForm($form_values, $short_name)
     {
+        $supervisor_user = Auth::user();
 
+        $form_id = Form::where('short_name', $short_name)->firstOrFail()->id;
+
+        foreach ($form_values['answers'] as $evaluation) {
+            $form_status_id = FormStatus::where('user_id', $supervisor_user->id)
+                ->where('form_id', $form_id)
+                ->firstOrFail()
+                ->id;
+
+            $form_answer = FormAnswer::where('form_status_id', $form_status_id)
+                ->join('users', 'users.id', '=', 'form_answers.evaluated_user_id')
+                ->where('role', 'student')
+                ->where('role_id', $evaluation['student_number'])
+                ->select('form_answers.id')
+                ->firstOrFail();
+
+            foreach ($evaluation['categorized_ratings'] as $ratings) {
+                foreach ($ratings as $rating_question_id => $score) {
+                    $form_rating_score = RatingScore::where('form_answer_id', $form_answer->id)
+                        ->where('rating_question_id', $rating_question_id)
+                        ->firstOrFail();
+                    $form_rating_score->score = $score;
+                    $form_rating_score->save();
+                }
+            }
+
+            foreach ($evaluation['opens'] as $open_question_id => $answer) {
+                $form_open_answer = OpenAnswer::where('form_answer_id', $form_answer->id)
+                    ->where('open_question_id', $open_question_id)
+                    ->firstOrFail();
+                $form_open_answer->answer = $answer;
+                $form_open_answer->save();
+            }
+        }
     }
 
     public function draftInternEvaluationForm(Request $request)
     {
+        $short_name = 'intern-evaluation';
+        $form_values = $request->validate([
+            'answers' => ['array'],
+            'answers.*.student_number' => ['integer', 'numeric'],
+            'answers.*.categorized_ratings' => ['array'],
+            'answers.*.categorized_ratings.*' => ['array'],
+            'answers.*.categorized_ratings.*.*' => ['nullable'],
+            'answers.*.opens' => ['array'],
+            'answers.*.opens.*' => ['nullable'],
+        ]);
 
+        $this->updateInternEvaluationForm($form_values, $short_name);
+
+        return redirect('/dashboard');
     }
 
     public function submitInternEvaluationForm(Request $request)
     {
+        $short_name = 'intern-evaluation';
+        $form_values = $request->validate([
+            'answers' => ['array'],
+            'answers.*.student_number' => ['integer', 'numeric'],
+            'answers.*.categorized_ratings' => ['array'],
+            'answers.*.categorized_ratings.*' => ['array'],
+            'answers.*.categorized_ratings.*.*' => ['required', 'integer', 'numeric'],
+            'answers.*.opens' => ['array'],
+            'answers.*.opens.*' => ['nullable', 'string'],
+        ]);
 
+        $this->updateInternEvaluationForm($form_values, $short_name);
+
+        $user = Auth::user();
+
+        // Update status for all reports under the supervisor
+        FormStatus::where('user_id', $user->id)
+            ->join('forms', 'forms.id', '=', 'form_statuses.form_id')
+            ->where('forms.short_name', $short_name)
+            ->update(['status' => 'submitted']);
+
+        return redirect('/dashboard');
     }
 
     public function validateForm(string $short_name, int $user_id): RedirectResponse
