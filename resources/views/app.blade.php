f8ec44b5957562a7c8a5fd02e755c71b65fbed44
Ehren Castillo <ehrencastillo@gmail.com>
2025-02-12 19:39:23 +0800

    feat: add page title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>Portalis: CS 195</title>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     @inertiaHead
 </head>
