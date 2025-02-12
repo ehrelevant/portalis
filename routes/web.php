fb0613645b0d3ae246b92e9080b92753771b3d0d
Ehren Castillo <ehrencastillo@gmail.com>
2025-02-12 19:22:03 +0800

    feat(admin): add routes for adding and deleting users and companies

--- routes/web.php
+++ routes/web.php
@@ -80,6 +80,18 @@ Route::middleware(['auth'])->group(function () {
     Route::middleware([EnsureUserHasRole::class . ':admin'])->group(function () {
         Route::get('/dashboard/admin/students', [AdminController::class, 'showStudents']);
         Route::get('/dashboard/admin/supervisors', [AdminController::class, 'showSupervisors']);
+        Route::get('/dashboard/admin/companies', [AdminController::class, 'showCompanies']);
+        Route::get('/dashboard/admin/faculties', [AdminController::class, 'showFaculties']);
+
+        Route::post('/add/student', [AdminController::class, 'addStudent']);
+        Route::post('/add/supervisor', [AdminController::class, 'addSupervisor']);
+        Route::post('/add/company', [AdminController::class, 'addCompany']);
+        Route::post('/add/faculty', [AdminController::class, 'addFaculty']);
+
+        Route::delete('/delete/student', [AdminController::class, 'deleteStudent']);
+        Route::delete('/delete/supervisor', [AdminController::class, 'deleteSupervisor']);
+        Route::delete('/delete/company', [AdminController::class, 'deleteCompany']);
+        Route::delete('/delete/faculty', [AdminController::class, 'deleteFaculty']);
     });
 
     Route::put('/globals/update-website-state', [WebsiteStateController::class, 'updateWebsiteState']);
