43e39cc91c7f15f42f33ae2d2c5c1c754049453c
Ehren Castillo <ehrencastillo@gmail.com>
2025-02-14 21:55:40 +0800

    feat: clean up home and privacy pages

--- resources/js/pages/privacy/Index.svelte
+++ resources/js/pages/privacy/Index.svelte
@@ -1,30 +1,28 @@
-<div class="main-screen w-full p-4">
+<div class="main-screen flex w-full flex-col gap-4 p-4">
     <div class="text-3xl font-semibold">Privacy Policy</div>
-</div>
-
-<div class="main-screen w-full p-4">
-    <div class="text-3xl font-semibold">Privacy Policy</div>
-    <div class="text-md">
-        In compliance with the Data Privacy Act of 2012 and the UP Diliman Privacy 
-        Policy, and all relevant data privacy regulations, Portalis: the CS 195 
-        portal, this web application, respects your personal data and ensures that 
-        it is protected. This Privacy Policy serves to inform you of the reason 
-        behind the collection of data, and the extent of which your data is 
-        processed. By logging in and using this application, you consent to 
-        processing of your information as described in this privacy policy. 
+    <div class="text-lg">
+        In compliance with the <strong>Data Privacy Act of 2012</strong>
+        and the UP Diliman Privacy Policy, and all relevant data privacy regulations,
+        <em>Portalis: the CS 195 portal</em>, this web application, respects
+        your personal data and ensures that it is protected. This Privacy Policy
+        serves to inform you of the reason behind the collection of data, and
+        the extent of which your data is processed. By logging in and using this
+        application, you consent to processing of your information as described
+        in this privacy policy.
         <br /> <br />
-        We only collect personal information that you have provided when using this 
-        application. Email addresses are used for the identification purposes of 
-        the user. Any other related data collected by this application will be used 
-        solely for the purpose of ensuring a smooth and seamless student experience 
-        regarding their internships. 
+        We only collect personal information that you have provided when using this
+        application. Email addresses are used for the identification purposes of
+        the user. Any other related data collected by this application will be used
+        solely for the purpose of ensuring a smooth and seamless student experience
+        regarding their internships.
         <br /> <br />
-        We do not sell or trade your personal information to third parties. 
+        We do not sell or trade your personal information to third parties.
         <br /> <br />
-        If you have any questions or concerns, please feel free to contact us 
-        through the following channels:
-        <br /> <br />
-        <br /> <br />
-        <br /> <br />
+        If you have any questions or concerns, please feel free to contact us through
+        the following email:
+        <a
+            href="mailto:portalis195@gmail.com"
+            class="text-floating-blue underline">portalis195@gmail.com</a
+        >
     </div>
 </div>
