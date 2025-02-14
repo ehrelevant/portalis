064cf05cf57b7fbbcbae0d086e50a881ff5f68f2
Ehren Castillo <ehrencastillo@gmail.com>
2025-02-14 22:28:16 +0800

    fix: fix seeders to actually contain the right factories

--- database/seeders/TestStudentSeeder.php
+++ database/seeders/TestStudentSeeder.php
@@ -2,8 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Faculty;
-use App\Models\Supervisor;
+use App\Models\Student;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,65 +14,94 @@ class TestStudentSeeder extends Seeder
      */
     public function run(): void
     {
-        Supervisor::factory()->create([
-            'company_id' => 1,
+        Student::factory()->create([
+            'student_number' => 202200000,
+            'supervisor_id' => 1,
+            'faculty_id' => null,
+            'grade' => null,
+            'wordpress_name' => 'student',
+            'wordpress_email' => 'student@example.com',
+            'has_dropped' => false,
         ]);
         User::factory()->create([
-            'email' => 'supervisor@example.com',
-            'role' => 'supervisor',
-            'role_id' => 1,
-            'first_name' => 'First',
-            'middle_name' => 'Middle',
-            'last_name' => 'Supervisor',
-        ]);
-
-        Supervisor::factory()->create([
-            'company_id' => 2,
-        ]);
-        User::factory()->create([
-            'email' => 'supervisor2@example.com',
-            'role' => 'supervisor',
-            'role_id' => 2,
-            'first_name' => 'Second',
-            'middle_name' => 'Middle',
-            'last_name' => 'Supervisor',
-        ]);
-
-        Faculty::factory()->create([
-            'section' => 'U1',
-        ]);
-        User::factory()->create([
-            'email' => 'faculty@example.com',
-            'role' => 'faculty',
-            'role_id' => 1,
-            'first_name' => 'First',
-            'middle_name' => 'Middle',
-            'last_name' => 'Faculty',
-        ]);
-
-        Faculty::factory()->create([
-            'section' => 'U2',
-        ]);
-        User::factory()->create([
-            'email' => 'faculty2@example.com',
-            'role' => 'faculty',
-            'role_id' => 2,
-            'first_name' => 'Second',
-            'middle_name' => 'Middle',
-            'last_name' => 'Faculty',
-        ]);
-
-        User::factory()->create([
-            'email' => 'admin@example.com',
-            'role' => 'admin',
-            'role_id' => 1,
+            'email' => 'student@example.com',
+            'role' => 'student',
+            'role_id' => 202200000,
             'first_name' => 'First',
             'middle_name' => 'Middle',
             'last_name' => 'Last',
         ]);
 
-        $this->call([
-            TestStudentSeeder::class,
+        Student::factory()->create([
+            'student_number' => 202200001,
+            'supervisor_id' => 2,
+            'faculty_id' => null,
+            'grade' => null,
+            'wordpress_name' => 'ehren',
+            'wordpress_email' => 'eacastillo1@up.edu.ph',
+            'has_dropped' => false,
+        ]);
+        User::factory()->create([
+            'email' => 'eacastillo1@up.edu.ph',
+            'role' => 'student',
+            'role_id' => 202200001,
+            'first_name' => 'Ehren',
+            'middle_name' => 'Middle',
+            'last_name' => 'Castillo',
+        ]);
+
+        Student::factory()->create([
+            'student_number' => 202200002,
+            'supervisor_id' => 2,
+            'faculty_id' => null,
+            'grade' => null,
+            'wordpress_name' => 'edzo',
+            'wordpress_email' => 'csacyatan1@up.edu.ph',
+            'has_dropped' => false,
+        ]);
+        User::factory()->create([
+            'email' => 'csacyatan1@up.edu.ph',
+            'role' => 'student',
+            'role_id' => 202200002,
+            'first_name' => 'Clyde Ambroz',
+            'middle_name' => 'Middle',
+            'last_name' => 'Acyatan',
+        ]);
+
+        Student::factory()->create([
+            'student_number' => 202200003,
+            'supervisor_id' => 2,
+            'faculty_id' => null,
+            'grade' => null,
+            'wordpress_name' => 'yuwen',
+            'wordpress_email' => 'elsaavedra@up.edu.ph',
+            'has_dropped' => false,
+        ]);
+        User::factory()->create([
+            'email' => 'elsaavedra@up.edu.ph',
+            'role' => 'student',
+            'role_id' => 202200003,
+            'first_name' => 'Eoghaine Czeriel',
+            'middle_name' => 'Middle',
+            'last_name' => 'Saavedra',
+        ]);
+
+        Student::factory()->create([
+            'student_number' => 202200004,
+            'supervisor_id' => 1,
+            'faculty_id' => null,
+            'grade' => null,
+            'wordpress_name' => 'lawrence',
+            'wordpress_email' => 'drbermudez1@up.edu.ph',
+            'has_dropped' => false,
+        ]);
+        User::factory()->create([
+            'email' => 'drbermudez1@up.edu.ph',
+            'role' => 'student',
+            'role_id' => 202200004,
+            'first_name' => 'Dominic Lawrence',
+            'middle_name' => 'Middle',
+            'last_name' => 'Bermudez',
         ]);
     }
 }
