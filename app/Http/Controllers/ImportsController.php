0ff3714a525480e0ddd4235320e82337418ed8a1
up26-dominic-lawrence-bermudez <114312907+up26-dominic-lawrence-bermudez@users.noreply.github.com>
2025-03-04 04:29:05 +0800

    feat: add rough backend functionality for importing student list

--- app/Http/Controllers/ImportsController.php
+++ app/Http/Controllers/ImportsController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use Inertia\Response;
-use PhpOption\None;
 
 class ImportsController extends Controller
 {
@@ -24,31 +25,84 @@ class ImportsController extends Controller
             abort(401);
         }
 
-        // todo: validate CSV type, but be flexible with MIME type
+        // validate CSV MIME type
         // worst case, CSV has MIME type text/plain, which is basically the same as a regular .txt file
+        // given a CSV file 1.csv, if it is renamed to 1.txt with no changes in data, then it would still be acceptable
         $request->validate([
             'file' => ['required', 'mimes:csv,txt'],
         ]);
 
-        $filepath = $request->file('file');
+        $filepath = $request->file('file')->store('list/students');
+        self::deleteAllStudents();
         self::importStudents($filepath);
 
         // todo: add confirmation? view csv before proceeding with upload?
         return redirect('/dashboard/admin/students');
     }
 
-    public function importStudents(string $csvPath): void
-    {   
-        // todo: add actual CSV import logic
-        /*
-        //$studentsCsv = fopen($csvPath, 'r');
-        //fclose($studentsCsv);
+    // todo: possibly move to AdminController?
+    public function deleteAllStudents(): void
+    {
+        $student_ids = DB::table('users')
+            ->where('role', 'student')
+            ->pluck('role_id');
+        
+        $admin_controller = new AdminController();
+        foreach ($student_ids as $student_id) {
+            $admin_controller->deleteStudent($student_id);
+        }
+        
+    }
 
-        $test = fopen('test.csv', 'w');
-        fputcsv($test, ['hello world']);
-        fputcsv($test, [$csvPath]);
-        fclose($test);
+    public function importStudents($csvPath): void
+    {   
+        // todo: clean up CSV importing (esp for non-local) (this path is not very good)
+        $studentsCsv = fopen('../storage/app/private/' . $csvPath, 'r');
+
+        // todo 1: allow dynamic order of columns in CSV
+        // todo 2: error handling (import validation)
+        /*
+            student_number
+            first_name
+            middle_name
+            last_name
+            email
+            wordpress_name
+            wordpress_email
         */
+        
+        // skip header row; might also be usable for todo 1/2
+        $studentCsvRow = fgetcsv($studentsCsv);
+        
+        $headers = array(
+            'student_number' => 0,
+            'first_name' => 1,
+            'middle_name' => 2,
+            'last_name' => 3,
+            'email' => 4,
+            'wordpress_name' => 5,
+            'wordpress_email' => 6
+        );
+
+        // loop through every student in row
+        while (($studentCsvRow = fgetcsv($studentsCsv)) !== FALSE) {      
+            $new_student = new Student();
+            $new_student->student_number = $studentCsvRow[$headers['student_number']];
+            $new_student->supervisor_id = null;
+            $new_student->faculty_id = null;
+            $new_student->wordpress_name = $studentCsvRow[$headers['wordpress_name']];
+            $new_student->wordpress_email = $studentCsvRow[$headers['wordpress_email']];
+            $new_student->save();
+
+            $new_user = new User();
+            $new_user->role = User::ROLE_STUDENT;
+            $new_user->role_id = $new_student->id;
+            $new_user->first_name = $studentCsvRow[$headers['first_name']];
+            $new_user->middle_name = $studentCsvRow[$headers['middle_name']] ?? '';
+            $new_user->last_name = $studentCsvRow[$headers['last_name']];
+            $new_user->email = $studentCsvRow[$headers['email']];
+            $new_user->save();
+        }
     }
 
     // ---
@@ -71,8 +125,8 @@ class ImportsController extends Controller
             'file' => ['required', 'mimes:csv,txt'],
         ]);
 
-        $filepath = $request->file('file');
-        self::importStudents($filepath);
+        $filepath = $request->file('file')->store('/list/supervisors');
+        self::importSupervisors($filepath);
 
         // todo: add confirmation? view csv before proceeding with upload?
         return redirect('/dashboard/admin/supervisors');
