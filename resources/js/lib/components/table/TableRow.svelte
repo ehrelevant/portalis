6e92fd52c428b539f3cef647f9d2c28bdcb3d309
Ehren Castillo <ehrencastillo@gmail.com>
2025-03-10 23:27:37 +0800

    feat: add row hover colors on table

--- resources/js/lib/components/table/TableRow.svelte
+++ resources/js/lib/components/table/TableRow.svelte
@@ -4,9 +4,13 @@
 </script>
 
 <tr
-    class="border-border {header ? 'bg-secondary' : 'border-t-2'} {disabled
-        ? 'bg-destructive/20'
-        : ''}"
+    class="border-border transition-colors {header
+        ? 'bg-secondary'
+        : 'border-t-2'} {disabled
+        ? 'bg-destructive/20 hover:bg-destructive/30'
+        : !header
+          ? 'hover:bg-accent/40'
+          : ''}"
 >
     <slot />
 </tr>
