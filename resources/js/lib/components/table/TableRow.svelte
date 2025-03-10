9b03d5e068e4504d40aa93105ee7c59695b707d2
Ehren Castillo <ehrencastillo@gmail.com>
2025-03-10 22:39:56 +0800

    feat: add row color to disabled student rows

--- resources/js/lib/components/table/TableRow.svelte
+++ resources/js/lib/components/table/TableRow.svelte
@@ -1,7 +1,12 @@
 <script>
     export let header = false;
+    export let disabled = false;
 </script>
 
-<tr class="border-border {header ? 'bg-secondary' : 'border-t-2'}">
+<tr
+    class="border-border {header ? 'bg-secondary' : 'border-t-2'} {disabled
+        ? 'bg-destructive/20'
+        : ''}"
+>
     <slot />
 </tr>
