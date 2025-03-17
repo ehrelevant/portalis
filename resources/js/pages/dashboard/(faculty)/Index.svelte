bbb1ff881bc6c9e60533f4b3223aee40e960262d
BluNuggets <eoczer@gmail.com>
2025-03-18 00:08:58 +0800

    refactor(faculty): implement shadcn-svelte for dashboard

--- resources/js/pages/dashboard/(faculty)/Index.svelte
+++ resources/js/pages/dashboard/(faculty)/Index.svelte
@@ -1,9 +1,11 @@
 <script>
     import { Link, useForm } from '@inertiajs/svelte';
     import Header from '$lib/components/InternshipHeader.svelte';
-    import Account from '$assets/account_logo.svelte';
-    import Accordion from '$lib/components/Accordion.svelte';
-
+    import Icon from "@iconify/svelte";
+    import { Button } from "$lib/components/ui/button";
+    import { Label } from "$lib/components/ui/label";
+    import { Input } from '$lib/components/ui/input';
+    import * as Select from "$lib/components/ui/select";
     const phases = ['pre', 'during', 'post'];
 
     export let currentPhase;
@@ -26,94 +28,125 @@
 </script>
 
 <div class="main-screen flex w-full flex-col gap-2 p-4">
-    <Header txt="Faculty Dashboard" />
+    <Header txt="Admin Dashboard" />
 
-    <div class="grid w-full grid-cols-1 gap-2 sm:grid-cols-2">
+    <div class="grid w-full grid-cols-1 gap-2 sm:grid-cols-3">
         <Link
             href="/dashboard/students"
-            class="flex w-full flex-col items-center justify-center rounded-xl bg-white p-3 text-2xl hover:opacity-80 dark:bg-black"
         >
-            <Account />
-            <p>Student List</p>
+            <Button
+                class="flex grow w-full h-full rounded-xl p-3 text-2xl bg-muted border-b-dark-primary"
+                variant="outline"
+            >
+                <div
+                    class="flex flex-col items-center justify-center"
+                >
+                    <Icon icon="ph:student" class="text-4xl"/>
+                    <Label class="text-2xl">Student List</Label>
+                </div>
+            </Button>
         </Link>
 
-        {#if currentPhase === 'during' || currentPhase === 'post'}
-            <Link
-                href="/dashboard/supervisors"
-                class="flex w-full flex-col items-center justify-center rounded-xl bg-white p-3 text-2xl hover:opacity-80 dark:bg-black"
+        <Link
+            href="/dashboard/supervisors"
+        >
+            <Button
+                class="flex grow w-full h-full rounded-xl p-3 text-2xl bg-muted border-b-dark-primary"
+                variant="outline"
             >
-                <Account />
-                <p>Supervisor List</p>
-            </Link>
-            <Link
-                href="/dashboard/companies"
-                class="flex w-full flex-col items-center justify-center rounded-xl bg-white p-3 text-2xl hover:opacity-80 dark:bg-black"
+                <div
+                    class="flex flex-col items-center justify-center"
+                >
+                    <Icon icon="mdi:account-tie-outline" class="text-4xl"/>
+                    <Label class="text-2xl">Supervisor List</Label>
+                </div>
+            </Button>
+        </Link>
+
+        <Link
+            href="/dashboard/companies"
+        >
+            <Button
+                class="flex grow w-full h-full rounded-xl p-3 text-2xl bg-muted border-b-dark-primary"
+                variant="outline"
             >
-                <Account />
-                <p>Company List</p>
-            </Link>
-        {/if}
+                <div
+                    class="flex flex-col items-center justify-center"
+                >
+                    <Icon icon="mdi:company" class="text-4xl"/>
+                    <Label class="text-2xl">Company List</Label>
+                </div>
+            </Button>
+        </Link>
     </div>
 
-    <div class="w-full rounded-xl bg-gray-900 p-8">
+    <div class="w-full rounded-xl bg-muted p-8 border border-input">
         <form
             class="flex flex-col gap-4"
             on:submit|preventDefault={saveSettings}
         >
-            <div class="grid grid-cols-[auto,1fr] items-center gap-2">
-                <h2 class="col-span-2 text-xl font-bold">Phase</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-[auto,1fr] items-center gap-2">
+                    <Label class="col-span-2 text-xl font-bold border-b-2 border-b-dark-primary">Phase</Label>
 
-                <label class="ml-4" for="phase">Website Phase</label>
-                <select
-                    name="phase"
-                    bind:value={$settingsForm.phase}
-                    class="bg-white p-2 text-light-primary-text dark:bg-dark-background dark:text-dark-primary-text"
-                >
-                    {#each phases as phase}
-                        <option selected={phase === currentPhase} value={phase}
-                            >{phase}-internship</option
-                        >
+                    <Label class="ml-4 text-md" for="phase">Website Phase</Label>
+                    <Select.Root
+                        selected={ $settingsForm.phase
+                            ? { label: $settingsForm.phase + "-internship", value: $settingsForm.phase}
+                            : undefined
+                        }
+                        onSelectedChange={(v) => {
+                        v && ($settingsForm.phase = v.value);
+                        }}
+                    >
+                        <Select.Trigger class="w-full">
+                            <Select.Value placeholder="{currentPhase}-internship" />
+                        </Select.Trigger>
+                        <Select.Content>
+                            {#each phases as phase}
+                                <Select.Item value={phase}>{phase}-internship</Select.Item>
+                            {/each}
+                        </Select.Content>
+                    </Select.Root>
+                
+                    <h2 class="col-span-2 mt-4 text-xl font-bold border-b-2 border-b-dark-primary">
+                        Requirement Deadlines
+                    </h2>
+                    {#each $settingsForm.requirements as requirement, i}
+                        {@const { requirement_name } = requirement}
+                        <Label class="ml-4 text-md" for="{requirement_name} deadline">
+                            {requirement_name}
+                        </Label>
+                        <Input
+                            name="{requirement_name} deadline"
+                            type="datetime-local"
+                            step="1"
+                            bind:value={$settingsForm.requirements[i].deadline}
+                            class="flex flex-col p-2 justify-between"
+                        />
                     {/each}
-                </select>
 
-                <h2 class="col-span-2 mt-4 text-xl font-bold">
-                    Requirement Deadlines
-                </h2>
-                {#each $settingsForm.requirements as requirement, i}
-                    {@const { requirement_name } = requirement}
-                    <label class="ml-4" for="{requirement_name} deadline">
-                        {requirement_name}
-                    </label>
-                    <input
-                        name="{requirement_name} deadline"
-                        type="datetime-local"
-                        step="1"
-                        bind:value={$settingsForm.requirements[i].deadline}
-                        class="bg-white p-2 text-light-primary-text dark:bg-dark-background dark:text-dark-primary-text"
-                    />
-                {/each}
-                <h2 class="col-span-2 mt-4 text-xl font-bold">
-                    Form Deadlines
-                </h2>
-                {#each $settingsForm.forms as form, i}
-                    {@const { form_name } = form}
-                    <label class="ml-4" for="{form_name} deadline">
-                        {form_name}
-                    </label>
-                    <input
-                        name="{form_name} deadline"
-                        type="datetime-local"
-                        step="1"
-                        bind:value={$settingsForm.forms[i].deadline}
-                        class="bg-white p-2 text-light-primary-text dark:bg-dark-background dark:text-dark-primary-text"
-                    />
-                {/each}
+                    <h2 class="col-span-2 mt-4 text-xl font-bold border-b-2 border-b-dark-primary">
+                        Form Deadlines
+                    </h2>
+                    {#each $settingsForm.forms as form, i}
+                        {@const { form_name } = form}
+                        <Label class="ml-4 text-md" for="{form_name} deadline">
+                            {form_name}
+                        </Label>
+                        <Input
+                            name="{form_name} deadline"
+                            type="datetime-local"
+                            step="1"
+                            bind:value={$settingsForm.forms[i].deadline}
+                            class="flex flex-col p-2 justify-between"
+                        />
+                    {/each}
             </div>
-            <input
+            <Button
                 type="submit"
-                value="Save"
-                class="w-full cursor-pointer rounded-full bg-light-primary p-2 hover:opacity-90 dark:bg-dark-primary"
-            />
+                variant="outline"
+                class="w-full cursor-pointer bg-dark-primary text-white text-xl p-2"
+            > Save </Button>
         </form>
     </div>
 </div>
