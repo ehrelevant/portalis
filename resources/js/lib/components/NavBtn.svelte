b76025249120bad8a66c5c10ea20f7b17e21191a
Ehren Castillo <ehrencastillo@gmail.com>
2025-03-18 04:33:21 +0000

    refactor(NavBtn): prefer inertia Links over anchor tags

--- resources/js/lib/components/NavBtn.svelte
+++ resources/js/lib/components/NavBtn.svelte
@@ -1,25 +1,26 @@
 <script>
     import { Link, page } from '@inertiajs/svelte';
-    import Icon from "@iconify/svelte";
-    import * as Tooltip from "$lib/components/ui/tooltip";
+    import Icon from '@iconify/svelte';
+    import * as Tooltip from '$lib/components/ui/tooltip';
 
     export let href = '#';
     export let icon_handle;
 
-    $: isActive = '/' + $page.url.split('/')[1] === href ? 'bg-dark-secondary text-dark-secondary-text' : 'hover:text-dark-primary-text text-dark-secondary';
+    $: isActive =
+        '/' + $page.url.split('/')[1] === href
+            ? 'bg-dark-secondary text-dark-secondary-text'
+            : 'hover:text-dark-primary-text text-dark-secondary';
 </script>
 
 <Tooltip.Root>
-    <Tooltip.Trigger asChild let:builder>
-        <a
-        href={href}
-        class="{isActive} flex h-9 w-9 items-center justify-center rounded-lg transition-colors md:h-8 md:w-8"
-        use:builder.action
-        {...builder}
+    <Tooltip.Trigger>
+        <Link
+            {href}
+            class="{isActive} flex h-9 w-9 items-center justify-center rounded-lg transition-colors md:h-8 md:w-8"
         >
-            <Icon icon={icon_handle} class="text-2xl"/>
+            <Icon icon={icon_handle} class="text-2xl" />
             <span class="sr-only"> <slot /> </span>
-        </a>
+        </Link>
     </Tooltip.Trigger>
-    <Tooltip.Content side="right"> <slot /> </Tooltip.Content>
+    <Tooltip.Content side="right"><slot /></Tooltip.Content>
 </Tooltip.Root>
