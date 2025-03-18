151b9685211c7f344db1af10aa334ac51ecea52e
Ehren Castillo <ehrencastillo@gmail.com>
2025-03-18 04:25:36 +0000

    feat: add flash notification when saving website settings

--- app/Http/Controllers/WebsiteStateController.php
+++ app/Http/Controllers/WebsiteStateController.php
@@ -5,39 +5,49 @@ namespace App\Http\Controllers;
 use App\Models\Form;
 use App\Models\Requirement;
 use App\Models\WebsiteState;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rule;
 
 class WebsiteStateController extends Controller
 {
     public function updateSettings(Request $request)
     {
-        $form_values = $request->validate([
-            'phase' => ['required', Rule::in(['pre','during','post'])],
-            'requirements.*.id' => ['int'],
-            'requirements.*.deadline' => ['date', 'nullable'],
-            'forms.*.id' => ['int'],
-            'forms.*.deadline' => ['date', 'nullable'],
-        ]);
+        try {
+            $form_values = $request->validate([
+                'phase' => ['required', Rule::in(['pre', 'during', 'post'])],
+                'requirements.*.id' => ['int'],
+                'requirements.*.deadline' => ['date', 'nullable'],
+                'forms.*.id' => ['int'],
+                'forms.*.deadline' => ['date', 'nullable'],
+            ]);
 
-        $website_state = WebsiteState::findOrFail(1);
-        $website_state->phase = $form_values['phase'];
-        $website_state->save();
+            $website_state = WebsiteState::findOrFail(1);
+            $website_state->phase = $form_values['phase'];
+            $website_state->save();
 
-        $new_requirements = $form_values['requirements'];
-        $new_forms = $form_values['forms'];
+            $new_requirements = $form_values['requirements'];
+            $new_forms = $form_values['forms'];
 
-        foreach ($new_requirements as $new_requirement) {
-            ['id' => $id, 'deadline' => $deadline] = $new_requirement;
-            $requirement = Requirement::find($id);
-            $requirement->deadline = $deadline;
-            $requirement->save();
-        }
-        foreach ($new_forms as $new_form) {
-            ['id' => $id, 'deadline' => $deadline] = $new_form;
-            $form = Form::find($id);
-            $form->deadline = $deadline;
-            $form->save();
+            foreach ($new_requirements as $new_requirement) {
+                ['id' => $id, 'deadline' => $deadline] = $new_requirement;
+                $requirement = Requirement::find($id);
+                $requirement->deadline = $deadline;
+                $requirement->save();
+            }
+            foreach ($new_forms as $new_form) {
+                ['id' => $id, 'deadline' => $deadline] = $new_form;
+                $form = Form::find($id);
+                $form->deadline = $deadline;
+                $form->save();
+            }
+
+            return back()->with('success', 'Successfully saved settings.');
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+
+            return back()->with('error', 'Failed to save settings.');
         }
     }
 }
