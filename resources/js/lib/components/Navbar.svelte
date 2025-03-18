220a52a5200d4bdac7be10bc1f71c78b42249442
Ehren Castillo <ehrencastillo@gmail.com>
2025-03-18 04:34:58 +0000

    fix(Navbar): avoid shrinking navbar on sparse pages

--- resources/js/lib/components/Navbar.svelte
+++ resources/js/lib/components/Navbar.svelte
@@ -1,36 +1,33 @@
 <script lang="ts">
-    import * as Tooltip from "$lib/components/ui/tooltip";
+    import * as Tooltip from '$lib/components/ui/tooltip';
     import NavBtn from './NavBtn.svelte';
     import Toggle from './ThemeSwitch.svelte';
-    import Menu from '$assets/menu_logo.svelte';
-    import Close from '$assets/x.svelte';
-    import { slide } from 'svelte/transition';
 
     let isMenuOpen = false;
-
-    function toggleMenu() {
-        isMenuOpen = !isMenuOpen;
-    }
 </script>
 
-<aside class="bg-dark-primary sticky inset-y-0 left-0 z-10 hidden max-h-screen min-h-full w-14 flex-col sm:flex">
+<aside
+    class="sticky inset-y-0 left-0 z-30 hidden max-h-screen min-h-full w-14 shrink-0 flex-col bg-dark-primary sm:flex"
+>
     <nav class="flex flex-col items-center gap-4 px-2 sm:py-5">
         <NavBtn href="/" icon_handle="tabler:home">Home</NavBtn>
-        <NavBtn href="/dashboard" icon_handle="ic:outline-dashboard">Dashboard</NavBtn>
+        <NavBtn href="/dashboard" icon_handle="ic:outline-dashboard"
+            >Dashboard</NavBtn
+        >
         <NavBtn href="/privacy" icon_handle="uil:lock">Privacy</NavBtn>
         <NavBtn href="/account" icon_handle="octicon:person-16">Account</NavBtn>
     </nav>
     <nav class="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-      <Tooltip.Root>
-        <Tooltip.Trigger>
-          <div
-            class="text-muted-foreground hover:text-foreground flex h-9 w-9 items-center justify-center rounded-lg transition-colors md:h-8 md:w-8"
-          >
-            <Toggle />
-            <span class="sr-only">Toggle Theme</span>
-        </div>
-        </Tooltip.Trigger>
-        <Tooltip.Content side="right">Toggle Theme</Tooltip.Content>
-      </Tooltip.Root>
+        <Tooltip.Root>
+            <Tooltip.Trigger>
+                <div
+                    class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                >
+                    <Toggle />
+                    <span class="sr-only">Toggle Theme</span>
+                </div>
+            </Tooltip.Trigger>
+            <Tooltip.Content side="right">Toggle Theme</Tooltip.Content>
+        </Tooltip.Root>
     </nav>
-  </aside>
+</aside>
