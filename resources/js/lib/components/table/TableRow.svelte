80d9c8df2dbf1cb33158b01ad15864a299b8a313
Ehren Castillo <ehrencastillo@gmail.com>
2025-03-24 11:50:49 +0800

    feat(TableRow): add selected flag

--- resources/js/lib/components/table/TableRow.svelte
+++ resources/js/lib/components/table/TableRow.svelte
@@ -1,12 +1,13 @@
 <script>
     export let header = false;
     export let disabled = false;
+    export let selected = false;
 </script>
 
 <tr
     class="z-40 border-border transition-colors {header
         ? 'sticky top-0 bg-secondary'
-        : 'border-b-2'} {disabled
+        : 'border-b-2'} {selected ? 'bg-primary/10' : ''} {disabled
         ? 'bg-destructive/20 hover:bg-destructive/30'
         : !header
           ? 'hover:bg-accent/40'
