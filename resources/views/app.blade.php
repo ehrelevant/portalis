093d3ed02b76adff1a95573dfa65d4066250b567
Ehren Castillo <ehrencastillo@gmail.com>
2025-03-25 16:13:27 +0800

    fix: use online fonts over importing local fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <title>Portalis: CS 195</title>
+    <link rel="stylesheet" href="https://code.cdn.mozilla.net/fonts/fira.css">
     @vite(['resources/js/app.ts', 'resources/css/app.css'])
     @inertiaHead
 </head>
