7a0cdde36870a882003bc5721022292d34769f96
Ehren Castillo <ehrencastillo@gmail.com>
2025-04-21 19:16:18 +0800

    fix: replace Label with p for non-form content

--- resources/js/pages/dashboard/(student)/Unenrolled.svelte
+++ resources/js/pages/dashboard/(student)/Unenrolled.svelte
@@ -1,16 +1,15 @@
 <script>
     import Header from '$lib/components/InternshipHeader.svelte';
-    import { Label } from '$lib/components/ui/label';
 </script>
 
 <div class="main-screen w-full p-4">
     <Header txt="You are currently NOT ENROLLED" />
 
     <div class="p-4">
-        <Label class="text-2xl">
+        <p class="text-2xl">
             You are currently registered as NOT ENROLLED to this course. This is
             likely due to a missing requirement. To resolve this, please contact the
             faculty or admin.
-        </Label>
+        </p>
     </div>
 </div>
