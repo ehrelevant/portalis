45738fd9234dc708499a0e5fbac2477e7c06b0a8
Ehren Castillo <ehrencastillo@gmail.com>
2025-05-07 04:51:01 +0800

    refactor(Table): prefer large tables for displaying data

--- resources/js/lib/components/table/Table.svelte
+++ resources/js/lib/components/table/Table.svelte
@@ -1,5 +1,5 @@
 <div
-    class="h-[28em] w-full border-collapse overflow-auto rounded-lg border-4 border-border pb-10"
+    class="h-screen w-full border-collapse overflow-auto rounded-lg border-4 border-border pb-10"
 >
     <table class="relative w-full border-collapse">
         <slot />
