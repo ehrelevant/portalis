5ad0c8fab9a983844d1f2ad29f63fb9e8aafd7b3
04nada <114312907+04nada@users.noreply.github.com>
2025-06-05 14:59:24 +0800

    refactor: set email rate limit to 30 per minute

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,9 @@ class AppServiceProvider extends ServiceProvider
         RateLimiter::for('pin_email', function (Request $request) {
             if (env('SEND_PIN_TO_EMAIL', false)) {
                 return [
-                    Limit::perHour(10)->by($request->ip()),
-                    Limit::perMinute(1)->by($request->ip()),
+                    // set rate limiter to 30 requests per minute
+                    Limit::perHour(1800)->by($request->ip()),
+                    Limit::perMinute(30)->by($request->ip()),
                 ];
             } else {
                 return Limit::none();
